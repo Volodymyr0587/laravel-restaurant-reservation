48d72d94ef527abbb9592c08ccbcbb2a16dd98c8
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-10-01 17:51:26 +0300

    display image on edit page form

--- resources/views/admin/menus/edit.blade.php
+++ resources/views/admin/menus/edit.blade.php
@@ -28,6 +28,9 @@
                         </div>
                         <div class="sm:col-span-6 pt-5">
                             <label for="image" class="block text-sm font-medium text-gray-700"> Image </label>
+                            <div>
+                                <img src="{{ $menu->image ? Storage::url($menu->image) : asset('images/menus/menu-default.jpg') }}" class="w-32 h-32 rounded">
+                            </div>
                             <div class="mt-1">
                                 <input type="file" id="image" name="image"
                                     class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
