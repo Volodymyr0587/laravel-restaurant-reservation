319e4a5a19e8de32775e738a730a3b27600cf186
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-10-02 16:03:52 +0300

    categories form: red border when error

--- resources/views/admin/menus/edit.blade.php
+++ resources/views/admin/menus/edit.blade.php
@@ -20,7 +20,8 @@
                             <label for="name" class="block text-sm font-medium text-gray-700"> Name </label>
                             <div class="mt-1">
                                 <input type="text" id="name" name="name"  value="{{ old('name', $menu->name) }}"
-                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5
+                                    @error('name') border-red-400 @enderror" />
                             </div>
                             @error('name')
                                 <div class="text-sm text-red-400">{{ $message }}</div>
@@ -33,7 +34,8 @@
                             </div>
                             <div class="mt-1">
                                 <input type="file" id="image" name="image"
-                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5
+                                    @error('image') border-red-400 @enderror" />
                             </div>
                             @error('image')
                                 <div class="text-sm text-red-400">{{ $message }}</div>
@@ -43,7 +45,8 @@
                             <label for="price" class="block text-sm font-medium text-gray-700"> Price </label>
                             <div class="mt-1">
                                 <input type="number" min="0.00" max="10000.00" step="0.01" id="price" name="price"  value="{{ old('price', $menu->price) }}"
-                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5
+                                    @error('price') border-red-400 @enderror" />
                             </div>
                             @error('price')
                                 <div class="text-sm text-red-400">{{ $message }}</div>
@@ -53,7 +56,8 @@
                             <label for="body" class="block text-sm font-medium text-gray-700">Description</label>
                             <div class="mt-1">
                                 <textarea id="body" rows="3" name="description"
-                                    class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">{{ old('description', $menu->description) }}</textarea>
+                                    class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('description') border-red-400 @enderror"
+                                    >{{ old('description', $menu->description) }}</textarea>
                             </div>
                             @error('description')
                                 <div class="text-sm text-red-400">{{ $message }}</div>
@@ -62,7 +66,7 @@
                         <div class="sm:col-span-6 pt-5">
                             <label for="categories" class="block text-sm font-medium text-gray-700">Categories</label>
                             <div class="mt-1">
-                                <select id="categories" name="categories[]" class="form-multiselect block w-full mt-1 rounded-md"
+                                <select id="categories" name="categories[]" class="form-multiselect block w-full mt-1 rounded-md @error('categories') border-red-400 @enderror"
                                     multiple>
                                     @foreach ($categories as $category)
                                         <option value="{{ $category->id }}" @selected($menu->categories->contains($category))>
