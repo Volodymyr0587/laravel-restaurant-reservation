39039986d104d04a83617dc161d54d8d42211227
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-10-04 18:08:14 +0300

    Delete commented flash message (all flash message in admin layout)

--- resources/views/admin/reservations/index.blade.php
+++ resources/views/admin/reservations/index.blade.php
@@ -2,20 +2,6 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Admin Dashboard') }} - {{ __('Reservations') }}
-            {{-- @if (session()->has('success'))
-                <div x-data="{show: true}"
-                    x-init="setTimeout(() => show = false, 4000)"
-                    x-show="show"
-                    class="mt-4 relative flex w-1/2 p-3 text-sm text-white bg-green-600 rounded-md">
-                    <p>
-                        {{ session('success') }}
-                    </p>
-                    <button @click="show = false"
-                        class="flex items-center justify-center transition-all w-8 h-8 rounded-md text-white hover:bg-white/10 active:bg-white/10 absolute top-1.5 right-1.5" type="button">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 w-5" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"></path></svg>
-                    </button>
-                </div>
-            @endif --}}
         </h2>
     </x-slot>
 
